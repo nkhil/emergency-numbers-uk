9f8c263e00909f651cffc14ac4f8cacc6a16cf2b
Nikhil Vijayan <nkhilv@gmail.com>
2024-08-24 17:41:55 +0100

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  Linking,
+  StatusBar,
+} from 'react-native'
+import { DATA, type EmergencyNumber } from './constants/data'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#E6C800" barStyle="dark-content" />
+      <Text style={styles.appHeader}>Emergency Numbers UK</Text>
+      <Text style={styles.appSubheader}>Tap on an emergency number to call</Text>
+      <FlatList
+        data={DATA}
+        renderItem={
+          ({item}) => <Item id={item.id} title={item.title} subTitle={item.subTitle} number={item.number} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
+
+const makeCall = (number: EmergencyNumber['number']) => {
+  console.log('Make call called with')
+  console.log(`number: ${String(number)}`)
+  Alert.alert(
+    "Call Confirmation",
+    `Do you want to call ${number}?`,
+    [
+      {
+        text: "Cancel",
+        style: "cancel"
+      },
+      {
+        text: "Call",
+        onPress: () => {
+          Linking.openURL(`tel:${number}`);
+        }
+      }
+    ]
+  )
+}
+
+const Item = ({title, subTitle, number }: EmergencyNumber) => (
+  <TouchableOpacity style={styles.itemContainer} onPress={() => makeCall(number)}>
+    <View style={styles.textContainer}>
+      <Text style={styles.name}>{title}</Text>
+      <Text style={styles.subtitle}>{subTitle}</Text>
+    </View>
+    <Text style={styles.number}>{number}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#E6C800', // ORANGE
+    // backgroundColor: '#DBB035', // ORANGE
     justifyContent: 'center',
   },
-});
+  appHeader: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    fontFamily: 'Helvetica',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    marginTop: 10,
+  },
+  appSubheader: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    fontFamily: 'Helvetica',
+    fontSize: 18,
+    marginBottom: 10,
+
+  },
+  name: {
+    fontSize: 20,
+    color: '#070708',
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#070708',
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+  },
+  number: {
+    fontSize: 20,
+    color: '#070708',
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#FFF',
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#FFF',
+    marginHorizontal: 10,
+    borderRadius: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  flag: {
+    width: 50,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+})
---
+++ constants/data.ts
@@ -0,0 +1,54 @@
+
+export type EmergencyNumber = {
+  id: string
+  title: string
+  subTitle?: string
+  number: string
+}
+
+export const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Police',
+    subTitle: '(emergency)',
+    number: '999',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Police',
+    subTitle: '(non-emergency)',
+    number: '101',
+  },
+  {
+    id: 'afa04346-69e6-44b0-ab10-ad3f04114612',
+    title: 'Transport Police',
+    subTitle: '(emergency)',
+    number: '999',
+  },
+  {
+    id: '219c34fa-1f4f-41b4-9447-19b2d25c0e99',
+    title: 'Transport Police',
+    subTitle: '(non-emergency)',
+    number: '+44800405040',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Fire Brigade',
+    number: '999',
+  },
+  {
+    id: 'a8e340f4-3a57-4047-9e10-b3d28e2f673f',
+    title: 'Coast Guard',
+    number: '999',
+  },
+  {
+    id: 'b00274ad-ac38-41eb-a3cb-34907bd4f5da',
+    title: 'Anti-terrorism hotline',
+    number: '0800 789 321',
+  },
+  {
+    id: '2061F017-2D06-44E3-9383-4FC7C0278F98',
+    title: 'Mountain Rescue',
+    number: '999',
+  },
+]
